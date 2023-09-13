c8d01764b3429e91e94baf1136dfe3f770d8e357
aidanaden <aidan@u.nus.edu>
2023-09-13 15:05:00 +0800

    feat: init

---
+++ utils.go
@@ -0,0 +1,32 @@
+package rotten_tomato
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func GetChunksFromString(startTag string, endTag string, content string) []string {
+	startTagLength := len(startTag)
+	endTagLength := len(endTag)
+	startTagReg := regexp.MustCompile(fmt.Sprintf(`%s(.*?)`, regexp.QuoteMeta(startTag)))
+	num_snippets := len(startTagReg.FindAllString(content, -1))
+	chunks := []string{}
+	j := 0
+	for i := 0; i < num_snippets; i++ {
+		rawChunk := content[j : len(content)-1]
+		start_idx := strings.Index(rawChunk, startTag)
+		end_idx := strings.Index(rawChunk, endTag)
+		if start_idx == -1 || end_idx == -1 {
+			continue
+		}
+		chunk := rawChunk[start_idx+startTagLength : end_idx]
+		chunks = append(chunks, chunk)
+		j += end_idx + endTagLength
+	}
+	return chunks
+}
+
+func RemoveSpecialChars(raw string) string {
+	return regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(raw, "")
+}
